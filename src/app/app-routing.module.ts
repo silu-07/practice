82b63bafd025918b602ee4179338da16767a7e3e
silu-07 <90642615+silu-07@users.noreply.github.com>
2024-04-18 20:55:39 +0530

    PageNotFound Comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { provideRouter, RouterModule, Routes, withComponentInputBinding } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { RoutingComponent } from './home/routing/routing.component';
 import { DefinitionsComponent } from './definitions/definitions.component';
 import {MatButtonModule} from '@angular/material/button'
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes : Routes = [
-  {path: '', component:HomeComponent},
-  {path: 'routing', component:RoutingComponent},
-  {path: 'definitions', component:DefinitionsComponent}
+  {path: '', component:HomeComponent, title:'Home Page'},
+  {path: 'routing', component:RoutingComponent, title: 'Ruting Page'},
+  {path: 'definitions', component:DefinitionsComponent},
+  {path: '**', component: PagenotfoundComponent}
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),MatButtonModule],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes, withComponentInputBinding())
+  ] 
 })
 export class AppRoutingModule {
   
